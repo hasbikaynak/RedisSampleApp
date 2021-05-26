dfcdfe8d8c80755b5c72528bd2e038a957f7fa36
M-Hasbi <kaynakhasbi1@gmail.com>
2021-05-26 14:24:35 +0200

    add delay task in WeatherForecastService.cs

--- RedisSample/Data/WeatherForecastService.cs
+++ RedisSample/Data/WeatherForecastService.cs
@@ -11,15 +11,16 @@ namespace RedisSample.Data
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            await Task.Delay(1500);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
+            }).ToArray();
         }
     }
 }
