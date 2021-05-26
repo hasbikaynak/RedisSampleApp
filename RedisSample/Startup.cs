9c7946d70e8d0044830598c1a23473e27a4e5a02
M-Hasbi <kaynakhasbi1@gmail.com>
2021-05-26 14:54:54 +0200

    add configuration and string connection code blogs in StartUp.cs

--- RedisSample/Startup.cs
+++ RedisSample/Startup.cs
@@ -29,6 +29,12 @@ namespace RedisSample
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration.GetConnectionString("Redis");
+                options.InstanceName = "RedisSample_";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
